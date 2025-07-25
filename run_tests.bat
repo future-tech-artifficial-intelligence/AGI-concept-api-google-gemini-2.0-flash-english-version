0c3ee5e51ad186b65a1ad67b98b7446ecf1971bb
future-tech-artifficial-intelligence <p8951775@gmail.com>
2025-07-25 18:04:12 +0200

    Update run_tests.bat

--- run_tests.bat
+++ run_tests.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Testing image analysis improvements...
-cd /d "c:\Users\bill\Desktop\AIMMMM\Project-AGI-ASI-AI-google-gemini-2.0-flash2-master"
+cd /d "enter the project path here"
 python test_image_analysis.py
 echo Test completed.
 pause
